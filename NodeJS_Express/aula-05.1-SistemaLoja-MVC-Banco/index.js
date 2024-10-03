eb72649ee1646cee9f911eaa7411f7729fd8143c
Ana Flávia V. Kunzendorff <142523426+akunzendorff@users.noreply.github.com>
2024-10-02 22:41:13 -0300

    Add files via upload

--- NodeJS_Express/aula-05.1-SistemaLoja-MVC-Banco/index.js
+++ NodeJS_Express/aula-05.1-SistemaLoja-MVC-Banco/index.js
@@ -1,18 +1,18 @@
 // Importando o Express com ES6 Modules
 import express from "express";
-
 // Iniciando o Express na variável app
 const app = express();
-
-//Importando o Sequelize com os dados da conexão
+// Importando o Sequelize (com os dados da conexão)
 import connection from "./config/sequelize-config.js";
-
 // Importando os Controllers (onde estão as rotas)
 import ClientesController from "./controllers/ClientesController.js";
 import ProdutosController from "./controllers/ProdutosController.js";
 import PedidosController from "./controllers/PedidosController.js";
 
-//Realizando a conexão com o banco de dados
+// Permite capturar dados vindos de formulários
+app.use(express.urlencoded({extended: false}));
+
+// Realizando a conexão com o banco de dados
 connection
   .authenticate()
   .then(() => {
@@ -22,11 +22,11 @@ connection
     console.log(error);
   });
 
-//Criando o banco de dados se ele não existir
+// Criando o banco de dados se ele não existir
 connection
   .query(`CREATE DATABASE IF NOT EXISTS loja;`)
   .then(() => {
-    console.log("O banco de dados está criado!");
+    console.log("O banco de dados está criado.");
   })
   .catch((error) => {
     console.log(error);
@@ -34,7 +34,6 @@ connection
 
 // Define o EJS como Renderizador de páginas
 app.set("view engine", "ejs");
-
 // Define o uso da pasta "public" para uso de arquivos estáticos
 app.use(express.static("public"));
 
